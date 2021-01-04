c4006c1c967751dd3cc7d743d1e37b668006e528
mezorian <mezorian@inc-ffh.solutions>
2021-01-04 23:54:36 +0100

    add js code to order containers differently on mobile devices, finish design of guest details section, start design of discussion section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const CONFIRMATION_STATUS = Object.freeze( {
+const CONFIRMATION_STATUS = Object.freeze({
   none: "none",
   confirmed: "confirmed",
   interested: "interested",
@@ -60,4 +60,26 @@ event.addGuest(guest);
 
 $("button").on("click", function() {
   alert("hello world");
-} );
+});
+
+function processMediaQueries(mediaQuery) {
+  // If media query matches (small screen)
+  if (mediaQuery.matches) {
+    // move content_discussion div into content_container_2
+    $("#content_discussion").detach().appendTo($("#content_container_2"))
+
+  // If media query does not match (wide screen)
+  } else {
+    // and if content_discussion is currently a child of content_container_2
+    if ($("#content_discussion").parent().length > 0) {
+      if ($("#content_discussion").parent().attr("id") === "content_container_2") {
+        // move content_discussion div back into content_container_1
+        $("#content_discussion").detach().appendTo($("#content_container_1"))
+      }
+    }
+  }
+}
+
+var mediaQuery = window.matchMedia("(max-width: 1100px)")
+processMediaQueries(mediaQuery) // Call listener function at run time
+mediaQuery.addListener(processMediaQueries) // Attach listener function on state changes
